5a455ee770f3b61e70de821f0f537545a24d5793
poonam2096740 <85899531+poonam2096740@users.noreply.github.com>
2022-02-11 16:37:21 +0530

    Update Compounds.js

--- js/Compounds.js
+++ js/Compounds.js
@@ -1,82 +1,103 @@
-AFRAME.registerComponent('atoms', {
-  init: async function(){
-    var compounds = await this.getCompounds()
-    console.log(compounds)
-    var barcodes = Object.keys(compounds)
-    console.log(barcodes)
+AFRAME.registerComponent("atoms", {
+  init: async function () {
+
+    //Get the compund details of the element
+    var compounds = await this.getCompounds();
+
+    var barcodes = Object.keys(compounds);
+
     barcodes.map(barcode => {
-      var element = compounds[barcode]
-      console.log(element)
-      this.createAtoms(element)
-    })
-  },
-  getCompounds: function(){
-    return fetch('js/compoundList.json')
-    .then(res => res.json())
-    .then(data => data)
-  },
-  getElementColors: function(){
-    return fetch('js/elementColors.json')
-    .then(res => res.json())
-    .then(data => data)
-  },
-  createAtoms: async function(element){
-    var elementName = element.element_name
-    var barcodeValue = element.barode_value
-    var numberOfElectrons = element.number_of_electron
-    var colors = await this.getElementColors()
-    var scene = document.querySelector('a-scene')
-    var marker = document.createElement('a-marker')
-    marker.setAttribute('id', `marker-${barcodeValue}`)
-    marker.setAttribute('type', 'barcode')
-    marker.setAttribute('element_name', elementName)
-    marker.setAttribute('value', barcodeValue)
-    scene.appendChild(marker)
+      var element = compounds[barcode];
 
-    var atom = document.createElement('a-entity')
-    atom.setAttribute('id', `${elementName}-${barcodeValue}`)
-    marker.appendChild(atom)
+      //Call the function
+      this.createAtoms(element);
+    });
 
-    var card = document.createElement('a-entity')
-    card.setAttribute('id', `card-${elementName}`)
-    card.setAttribute('geoometry', {
-      primitive: 'plane',
+  },
+  getCompounds: function () {
+    return fetch("js/compoundList.json")
+      .then(res => res.json())
+      .then(data => data);
+  },
+  getElementColors: function () {
+    return fetch("js/elementColors.json")
+      .then(res => res.json())
+      .then(data => data);
+  },
+  createAtoms: async function (element) {
+
+    //Element data
+    var elementName = element.element_name;
+    var barcodeValue = element.barcode_value;
+    var numOfElectron = element.number_of_electron;
+
+    //Get the color of the element
+    var colors = await this.getElementColors();
+
+    //Scene
+    var scene = document.querySelector("a-scene");
+
+    //Add marker entity for BARCODE marker
+    var marker = document.createElement("a-marker");
+
+    marker.setAttribute("id", `marker-${barcodeValue}`);
+    marker.setAttribute("type", "barcode");
+    marker.setAttribute("element_name", elementName);
+    marker.setAttribute("value", barcodeValue);
+
+    scene.appendChild(marker);
+
+    var atom = document.createElement("a-entity");
+    atom.setAttribute("id", `${elementName}-${barcodeValue}`);
+    marker.appendChild(atom);
+
+    //Create atom card
+    var card = document.createElement("a-entity");
+    card.setAttribute("id", `card-${elementName}`);
+    card.setAttribute("geometry", {
+      primitive: "plane",
       width: 1,
       height: 1
-    })
-    card.setAttribute('material', {
+    });
+
+    card.setAttribute("material", {
       src: `./assets/atom_cards/card_${elementName}.png`
-    })
-    card.setAttribute('position', {x: 0, y: 0, z: 0})
-    card.setAttribute('rotation', {x: -90, y: 0, z: 0})
-    atom.appendChild(card)
+    });
 
-    console.log('yess the code works!')
+    card.setAttribute("position", { x: 0, y: 0, z: 0 });
+    card.setAttribute("rotation", { x: -90, y: 0, z: 0 });
 
-    var nucleusRadius = 0.2
-    var nucleus = document.createElement('a-entity')
-    nucleus.setAttribute('id', `nucleus-${elementName}`)
-    nucleus.setAttribute('geometry', {
-      primitive: 'sphere',
+    atom.appendChild(card);
+
+    //Create nucleus
+    var nucleusRadius = 0.2;
+    var nucleus = document.createElement("a-entity");
+    nucleus.setAttribute("id", `nucleus-${elementName}`);
+    nucleus.setAttribute("geometry", {
+      primitive: "sphere",
       radius: nucleusRadius
-    })
-    nucleus.setAttribute('material', 'color', colors[elementName])
-    nucleus.setAttribute('position', {x: 0, y: 1, z: 0})
-    nucleus.setAttribute('rotation', {x: 0, y: 0, z: 0})
-    
-    var nucleusName = document.createElement('a-entity')
-    nucleusName.setAttribute('id', `nucleus-name-${elementName}`)
-    nucleusName.setAttribute('position', {x: 0, y: 0.2, z: -0.06})
-    nucleusName.setAttribute('rotation', {x: -90, y: 0, z: 0})
-    nucleusName.setAttribute('text', {
-      value: elementName,
-      font: 'monoid',
+    });
+
+    nucleus.setAttribute("material", "color", colors[elementName]);
+    nucleus.setAttribute("position", { x: 0, y: 1, z: 0 });
+
+    nucleus.setAttribute("rotation", { x: 0, y: 0, z: 0 });
+
+    var nucleusName = document.createElement("a-entity");
+    nucleusName.setAttribute("id", `nucleus-name-${elementName}`);
+    nucleusName.setAttribute("position", { x: 0, y: 0.21, z: -0.06 });
+    nucleusName.setAttribute("rotation", { x: -90, y: 0, z: 0 });
+    nucleusName.setAttribute("text", {
+      font: "monoid",
       width: 3,
-      color: 'black',
-      align: 'center'
-    })
-    nucleus.appendChild(nucleusName)
-    atom.appendChild(nucleus)
+      color: "black",
+      align: "center",
+      value: elementName
+    });
+
+    nucleus.appendChild(nucleusName);
+
+    atom.appendChild(nucleus);
 
     var orbitAngle = -180;
     var electronAngle = 30;
@@ -149,6 +170,8 @@ AFRAME.registerComponent('atoms', {
       });
 
       electronGroup.appendChild(electron);
+
+
     }
-  },
-})
+  }
+});
